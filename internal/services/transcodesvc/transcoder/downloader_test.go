9409ef67bbd66bbf7f7dc353314e64ac18289fed
thienthn <tutybukuny@gmai.com>
2026-08-23 15:07:20 +0700

    Fix: add downloader test

--- internal/services/transcodesvc/transcoder/downloader_test.go
+++ internal/services/transcodesvc/transcoder/downloader_test.go
@@ -1,8 +1,74 @@
 package transcoder
 
 import (
+	"bytes"
+	"context"
+	"fmt"
+	"os"
 	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"transcode-demo/config"
+	"transcode-demo/pkg/aws"
+	"transcode-demo/pkg/logger"
+	"transcode-demo/pkg/utils"
 )
 
 func TestS3Downloader_Download(t *testing.T) {
+	ctx := context.Background()
+	cfg := config.MustNewDevConfig(t)
+	l := logger.New(cfg.Log)
+
+	data := utils.RandString()
+	filePath := fmt.Sprintf("s3://local-bucket/%s", utils.RandString())
+	require.NoError(t, aws.Write(ctx, cfg.AWS, filePath, []byte(data)))
+
+	for _, te := range []struct {
+		name           string
+		filePath       string
+		expectedErrStr string
+	}{
+		{
+			name:     "valid_file",
+			filePath: filePath,
+		},
+		{
+			name:           "invalid_path",
+			filePath:       "invalid-path",
+			expectedErrStr: aws.ErrInvalidS3URL.Error(),
+		},
+		{
+			name:           "not_exist_file",
+			filePath:       "s3://local-bucket/not-exist-file",
+			expectedErrStr: "NoSuchKey",
+		},
+		{
+			name:           "invalid_bucket",
+			filePath:       "s3://invalid-bucket/test-file",
+			expectedErrStr: "The specified bucket does not exist",
+		},
+	} {
+		t.Run(te.name, func(t *testing.T) {
+			downloader := newS3Downloader(l, cfg.AWS)
+			outputPath, clean, err := downloader.Download(ctx, te.filePath)
+			if te.expectedErrStr != "" {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), te.expectedErrStr)
+				return
+			}
+			require.NoError(t, err)
+			require.NotEmpty(t, outputPath)
+			file, err := os.Open(outputPath)
+			require.NoError(t, err)
+			defer file.Close()
+			buf := bytes.NewBuffer(nil)
+			_, err = buf.ReadFrom(file)
+			require.NoError(t, err)
+			require.Equal(t, data, string(buf.Bytes()))
+			require.NoError(t, file.Close())
+			clean()
+			require.NoFileExists(t, outputPath)
+		})
+	}
 }
